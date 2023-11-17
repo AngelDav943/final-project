2f7dd0bfa6a80b38a66320a21e071cd3b57d5293
Angel <angel91.addcs@gmail.com>
2023-11-17 18:45:59 -0500

    base

---
+++ src/components/note.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function(props)
+{
+
+    function onClick()
+    {
+        if (props.onClick) props.onClick(props.text)
+    }
+
+    return (
+        <article className="note">
+            <p>{props.text}</p>
+            <button onClick={onClick}>Delete</button>
+        </article>
+    )
+}
